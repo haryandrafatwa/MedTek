2e20d32b4e3a9d9fbe4b1892f88c2f8b556d24b5
haryandrafatwa <haryandrafatwa010@gmail.com>
2020-07-08 23:05:14 +0700

    Initial Commit

--- app/src/main/java/com/example/medtek/Api.java
+++ app/src/main/java/com/example/medtek/Api.java
@@ -1,21 +1,9 @@
 package com.example.medtek;
 
-import com.google.gson.JsonObject;
-
 import org.json.JSONObject;
 
-import java.util.Map;
-
 import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FieldMap;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.Header;
-import retrofit2.http.HeaderMap;
 import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
